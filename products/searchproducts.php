e0ef3bd60090cb4334885f516d648ffcab3364cc
Sara-Sofia <n2kesa00@students.oamk.fi>
2023-04-26 08:29:40 +0300

    Search tehty

--- products/searchproducts.php
+++ products/searchproducts.php
@@ -5,11 +5,11 @@ require_once '../inc/headers.php';
 $url = $_SERVER['REQUEST_URI'];
 $parameters = explode('/', $url);
 $phrase = $parameters[3];
-
-
+//print_r($phrase);
+//print_r($parameters);
 try {
     $db = openDb();
-    $sql = "select * from Tuoteryhma where tuoteryhma_nimi like '%phrase%'";
+    $sql = "select * from Tuotteet where tuotteen_nimi like '%$phrase%'";
     selectAsJson($db,$sql);
 }
 catch(PDOException $pdoex) {
